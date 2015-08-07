6da338241aedb14e0685edca1fe1f7df28a09b89
Robert Rees <robert.rees@guardian.co.uk>
2015-08-07 19:07:17 +0100

    Makes the sample tests a bit more realistic

--- test/tests.js
+++ test/tests.js
@@ -1,7 +1,17 @@
-var assert = require("assert")
+var assert = require("chai").assert;
 
-describe('Link checker', function() {
-  it('should identify preview links', function() {
-    assert(true);
+require('node-amd-require')({
+  baseUrl: __dirname
+});
+
+var plugin = require('../src/scribe-plugin-link-prompt-command.js');
+
+describe('Plugin', function() {
+  it('should return a function', function() {
+    assert.isFunction(plugin);
+  });
+
+  it('should accept options', function() {
+    assert.isFunction(plugin({}));
   });
 });
