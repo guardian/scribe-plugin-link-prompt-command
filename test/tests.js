57a7b60d39a544be8e1746be66f98937257c8b56
Robert Rees <robert.rees@guardian.co.uk>
2015-10-27 12:31:08 +0000

    Creates a check to detect empty, whitespace input and the associated tests

--- test/tests.js
+++ test/tests.js
@@ -5,6 +5,7 @@ require('node-amd-require')({
 });
 
 var plugin = require('../src/scribe-plugin-link-prompt-command.js');
+var checks = require('../src/checks.js');
 
 describe('Plugin', function() {
   it('should return a function', function() {
@@ -17,5 +18,21 @@ describe('Plugin', function() {
 
   it('should accept a validation function', function() {
     assert.isFunction(plugin({validation: function(url) {return undefined; }}));
+  });
+});
+
+describe('Checks', function() {
+  describe('emptyLink', function() {
+    it('should reject strings containing only whitespace', function() {
+      ['\n\n', '   \t', ' ', ''].forEach(function(invalidLink) {
+        assert.isFalse(checks.emptyLink(invalidLink), invalidLink + ' was considered valid');
+      });
+    });
+
+    it('should consider genuine links as valid', function() {
+      ['http://www.theguardian.com', 'https://www.theguardian.com'].forEach(function(validLink) {
+        assert.isTrue(checks.emptyLink(validLink), validLink + ' was considered invalid');
+      });
+    });
   });
 });
